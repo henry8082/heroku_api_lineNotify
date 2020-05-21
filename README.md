e769391c8b223550302a954e5f5b6328276e97a3
henry <q355261@gmail.com>
2020-05-21 14:57:43 +0800

    0521 local suss

---
+++ README.md
@@ -0,0 +1,84 @@
+## ##基本說明
+
+-這是一個利用python的flask 開啟LINE Front-end Framework(liff)的liff.html檔
+
+當輸入提醒事項、日期、時間後經由ajax傳送至heroku_app.py 
+
+所寫的flask API Endpoint post /store 並將此資料傳送至Heroku Postgres資料庫中儲存
+
+最後在所設定的時間以Line Notifiy推播
+
+### -使用 heroku/Flask/html/ajax/Postgres資料庫/line notify
+
+<img src="https://github.com/henry8082/heroku_api_lineNotify/blob/master/img/S__64241710.jpg" width = "25%" /> <img src="https://github.com/henry8082/heroku_api_lineNotify/blob/master/img/S__64241711.jpg" width = "30%" />
+
+---------------------------------------
+### 重要設定：
+
+#### heroku_app.py  - 設定Flask API、html網頁
+
+#### clock.py - 設定排成，讓Line Notifiy推播
+
+---------------------
+
+HTML檔是放在templates中的liff.html
+
+在heroku_app.py中設定endpoint 的 route為liff時 
+
+利用render_template('liff.html')讓先建置好的HTML模板在網頁上顯示
+
+並在Procfile中設定
+<code>web: gunicorn -b :$PORT heroku_app:app --preload</code>
+
+這一行指令分成兩個部分，其格式 <process_type>: <command> 表示：
+
+啟用名為 web 的應用
+用 gunicorn 執行 mysite.wsgi 這個模組
+Gunicorn 是一個用 Python 開發的 WSGI 工具，可以用來執行 Flask 的網站。
+
+---------------------------------------
+因為在heroku上所部屬的檔案會在不使用時間超過30分鐘後自動進行休眠
+
+並於下次重開時重置所有的資料回初始值
+
+如果你將post上去的資料存在py檔內的list中，休眠後資料就會消失
+
+所以使用Heroku Postgres資料庫來儲存我們所post上來的資料
+
+並且讓Line Notifiy從資料庫抓取設定時間的資料，並推播
+
+---------------------------------------
+因推播時間為臺灣時區，跟heroku的時區不同
+
+因此要另外在Config Vars設定下列設置
+
+<code>TZ = Asia/Taipei</code>
+
+---------------------------------------
+最後利用apscheduler設定排程(<a href="https://github.com/maloyang/heroku-clock-howto">heroku上設定排程的參考網址</a>)
+
+在clock.py中設定在早上8點喚醒heroku
+
+```python
+sched.add_job(wakeup, 'cron', day_of_week='mon-sun',hour=8)
+```
+
+並於每分鐘都向資料庫取資料，來判斷資料庫裡面的時間是否與當前時間相同，如相同則會推播所提醒的事項
+
+```python
+@sched.scheduled_job('cron', day_of_week='mon-sun',hour='8-23', minute='*/1')
+def scheduled_job():
+```
+
+因為heroku每30分鐘就會休眠，因此設定一個wakup的排程，讓此排程每25分鐘開啟liff網頁來喚醒heroku
+
+達到這期間不休眠的效果
+
+```python
+@sched.scheduled_job('cron', day_of_week='mon-sun',hour='8-23', minute='*/25')
+def wakeup():
+```
+
+
+
+***
--- heroku_app.py
+++ heroku_app.py
@@ -3,71 +3,103 @@ import os
 from flask_cors import CORS
 import psycopg2
 
-def insert(event,date,time):
-    DATABASE_URL = os.environ['DATABASE_URL']
+def insert_update(sql):
+    #DATABASE_URL = os.environ['DATABASE_URL']
+    DATABASE_URL = os.popen('heroku config:get DATABASE_URL -a henry-json-server').read()[:-1]
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     cursor = conn.cursor()
     
-    cursor.execute("insert into events(event,date,time) values('{}','{}','{}');".format(event,date,time))
+    cursor.execute(sql)
         
     conn.commit()
     cursor.close()
     conn.close()
-
+    
+def Postgres_db(sql):
+    #DATABASE_URL = os.environ['DATABASE_URL']
+    DATABASE_URL = os.popen('heroku config:get DATABASE_URL -a henry-json-server').read()[:-1]
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    cursor = conn.cursor()
+    
+    cursor.execute(sql)
+    fetchall = cursor.fetchall() 
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return fetchall
 
 app = Flask(__name__)
 CORS(app)
-stores = []
+events = []
 
 #get html
 @app.route('/liff')
 def get_html():
     return render_template('liff.html')
 
+#get html
+@app.route('/table')
+def get_html_table():
+    sql = "select * from events"
+    content = Postgres_db(sql)
+    content = [(str(i[0]),i[1],i[2],i[3]) for i in content]
+    sqlid = {}
+    for i in content:
+        sqlid[str(i[0])] = True
+        
+    labels = ["資料庫編號","提醒事項","日期","時間"]
+        
+    return render_template('table.html', labels = labels, content=content,sqlid =sqlid)
 
-#get /store
-@app.route('/store')
-def get_stores():
-    return jsonify(stores)
-@app.route('/store/<string:name>')
-def get_store(name):
-    for store in stores:
-        if store['name'] == name:
-            return jsonify(store)
-        return jsonify ({'message': 'store not found'})
-    
-#get /store/<name>/item data: {name :}
-@app.route('/store/<string:name>/item')
-def get_item_in_store(name):
-    for store in stores:
-        if store['name'] == name:
-            return jsonify( {'items':store['items'] } )
-    return jsonify ({'message':'store not found'})
-#post /store data: {name :}
-@app.route('/store' , methods=['POST'])
+
+
+#get /event
+@app.route('/event')
+def get_events():
+    sql = "select * from events"
+    content = Postgres_db(sql)
+    sql2 = "SELECT column_name FROM information_schema.columns WHERE table_name = 'events'"
+    labels = Postgres_db(sql2)
+    labels = [l[0] for l in labels[1:]]
+    for i in content:
+        dic = {labels[0]:i[1],labels[1]:i[2],labels[2]:i[3]}
+        events.append(dic)
+    return jsonify(events)
+
+#put  
+@app.route('/table', methods=['GET','PUT'])
+def put_event():
+    request_data = request.get_json()
+
+    insert_update("update events set event='{}', date='{}',time='{}' where user_id={};".format(request_data['event'],request_data['date'],request_data['time'],request_data['sqlid']))
+           
+    return jsonify(events)
+
+#delete
+@app.route('/table', methods=['DELETE'])
+def delete_event():
+    request_data = request.get_json()
+
+    insert_update("delete from events where user_id={};".format(request_data['sqlid']))
+           
+    return jsonify(events)
+
+#post /event 
+@app.route('/event' , methods=['POST'])
 def create_event():
     request_data = request.get_json()
-    new_store = {
+    
+    new_event = {
         'event':request_data['event'],
         'date':request_data['date'],
         'time':request_data['time']
     }
-    insert(request_data['event'],request_data['date'],request_data['time'])
-    stores.append(new_store)
+    insert_update("insert into events(event,date,time) values('{}','{}','{}');".format(request_data['event'],request_data['date'],request_data['time']))
+    events.append(new_event)
     
-    return jsonify(new_store)
-#post /store/<name> data: {name :}
-@app.route('/store/<string:name>/item' , methods=['POST'])
-def create_item_in_store(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store['name'] == name:
-            new_item = {
-                'name': request_data['name'],
-                'price': request_data['price']
-            }
-        store['items'].append(new_item)
-        return jsonify(new_item)
+    return jsonify(new_event)
     
-app.run(host=os.getenv('IP', '0.0.0.0'), 
-        port=int(os.getenv('PORT', 4444)))
+    
+app.run()
+#host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 4444)) 
+        
